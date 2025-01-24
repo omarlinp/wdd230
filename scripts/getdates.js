de985288b054c3be6008a692e094b78bc6e3728a
omarlinp <omarlinp@gmail.com>
2025-01-23 23:19:26 -0600

    added the hamburger menu and the map

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,11 @@
 let o_last_modif = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML= o_last_modif
+
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
